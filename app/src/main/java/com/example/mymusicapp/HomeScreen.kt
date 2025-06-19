4bd2334a4733242e063b7476065fbf7640521e73
Prince04-28 <ST10494139@rcconnect.edu.za>
2025-06-19 13:29:27 +0200

    Functionality

--- app/src/main/java/com/example/mymusicapp/HomeScreen.kt
+++ app/src/main/java/com/example/mymusicapp/HomeScreen.kt
@@ -5,6 +5,7 @@ package com.example.mymusicapp
 import android.annotation.SuppressLint
 import android.app.Activity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,9 +26,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.height
+
+private fun Nothing?.navigate(string: String) {
+    TODO("Not yet implemented")
+}
 
 object HomeScreen {
+
+    private fun ColumnScope.Button(onClick: @Composable () -> Unit) {
+        TODO("Not yet implemented")
+    }
 
     @SuppressLint("ContextCastToActivity")
     @Composable
@@ -50,8 +58,11 @@ object HomeScreen {
         var comment by remember {
             mutableStateOf("")
         }
+        var Song by remember {
+            mutableStateOf("") }
 
         Column(modifier = Modifier.padding(16.dp)) {
+            ->
             Text("Add Song to Playlist", style = MaterialTheme.typography.h(.6))
             OutlinedTextField(
                 value = title,
@@ -77,34 +88,33 @@ object HomeScreen {
                 if (title.isNotEmpty() && artist.isNotEmpty() && rating.toIntOrNull() in 1..5) {
                     val songList = null
                     songList.run {
-                        add(value = this@HomeScreen.Song(
-                            title, artist, rating.toInt(), comment
-                        ))
+                        ->
+                        add(
+                            value = this@HomeScreen.Song(title), artist, rating.toInt(), comment
+                            )
+                        }
                     }
-                    title : String = artist = rating : String = comment : String  = ""
-                }
-            }) {
-                Text("Add to Playlist")
-            }
 
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = { navController.navigate("details") }) {
-                Text("Go to Details")
-            }
+                })
 
-            Spacer(modifier = with(Modifier) { height(8.dp) })
-            Button(onClick = { android.os.Process.killProcess(android.os.Process.myPid()) }) {
-                Text("Exit App")
             }
-        }
+        Text("Add to Playlist")
     }
+
+
+
+    @Composable
+    fun Song(x0: String) {
+        TODO("Not yet implemented")
+    }
+}
 
     @Composable
     fun Song(x0: String, x1: String, x2: Int, x3: String) {
         TODO("Not yet implemented")
     }
 
-    private fun Nothing?.add(value: Any) {
+    private fun Nothing?.add(value: Unit, artist: String, toInt: Int, comment: String) {
         TODO("Not yet implemented")
     }
 
@@ -117,7 +127,8 @@ object HomeScreen {
 
         return TODO("Provide the return value")
     }
-}
+
+
 
 
 
--- app/src/main/java/com/example/mymusicapp/SecondScreen.kt
+++ app/src/main/java/com/example/mymusicapp/SecondScreen.kt
@@ -3,23 +3,25 @@ package com.example.mymusicapp
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 
 object SecondScreen {
 
     @Composable
     fun SecondScreen(navController: NavController) {
-
-
-
-
-
-
 
         Column(
             modifier = Modifier
@@ -39,4 +41,43 @@ object SecondScreen {
             }
         }
     }
-}
+
+    private fun Typography.h(d: Double): TextStyle {
+
+        return TODO("Provide the return value")
+    }
+
+    @Composable
+    fun SecondScreen(navController: NavHostController) {
+        var average by remember { mutableStateOf(0.0) }
+
+        var title by remember {
+            mutableStateOf("")
+        }
+
+        var artist by remember {
+            mutableStateOf("")
+        }
+
+        var rating by remember {
+            mutableStateOf("")
+        }
+
+        var comment by remember {
+            mutableStateOf("")
+        }
+
+        val value = Text(
+            "Playlist Details", style = MaterialTheme.run { typography.h(.6) }
+        )}}
+
+
+
+
+
+
+
+
+
+
+
